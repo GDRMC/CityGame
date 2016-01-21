5981a194a29e4011bc83bcb9f37907dbaf60e8c9
GDRMc <gdrdu86@gmail.com>
2016-01-21 16:53:48 +0100

    Updated, now uses Sea Glass look and feel 0.2 bug fixed: line separators of the textArea has been turned off

--- src/gdr/citygame/launcher/LauncherDebug.java
+++ src/gdr/citygame/launcher/LauncherDebug.java
@@ -4,17 +4,30 @@ import gdr.citygame.plate.*;
 import gdr.citygame.player.*;
 import gdr.citygame.debug.*;
 import gdr.citygame.gamerules.*;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class LauncherDebug {
-    
-    public static void main(String[]args){
+
+    public static void main(String[] args) {
+        //initialize console instance for debug
         ConsoleHandler debug = ConsoleHandler.getInstance();
-                
-        Land c1 = new Land("Passerelle des Arts",1600000);
+
+        debug.dProgramHeader();
+
+        try {
+            UIManager.setLookAndFeel("com.seaglasslookandfeel.SeaGlassLookAndFeel");
+            System.setProperty("SeaGlass.JTextArea.drawLineSeparator", "false");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //TESTS
+        Land c1 = new Land("Passerelle des Arts", 1600000);
         c1.setBuildingValues(1000000, 120000, 600000, 1800000, 5000000, 7000000, 9000000);
         c1.verifyCardIntegrity();
         System.out.println(c1.toString());
         debug.dCard(c1);
     }
-    
+
 }
