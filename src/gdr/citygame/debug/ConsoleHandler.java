dc86004d1ed6c669075a7c4c344ad3a5545d7a42
GDRMc <gdrdu86@gmail.com>
2016-01-21 16:52:43 +0100

    Updated ConsoleHandler, now supports Prompt class and makes the bridge between Prompt class and the rest of the program

--- src/gdr/citygame/debug/ConsoleHandler.java
+++ src/gdr/citygame/debug/ConsoleHandler.java
@@ -1,10 +1,12 @@
 package gdr.citygame.debug;
 
+import gdr.citygame.frames.Prompt;
 import gdr.citygame.plate.*;
 
 public class ConsoleHandler {
 
     private static ConsoleHandler console;
+    private static Prompt window;
 
     private ConsoleHandler() {
         
@@ -13,16 +15,20 @@ public class ConsoleHandler {
     public static synchronized ConsoleHandler getInstance() {
         if (console == null) {
             console = new ConsoleHandler();
+            window = new Prompt();
+            window.setVisible(true);
         }
         return console;
     }
 
     public void d(String s){
         System.out.print(s);
+        this.window.write(s);
     }
     
     public void dln(String s){
         System.out.println(s);
+        this.window.writeln(s);
     }
     
     public void dCard(Land l){
@@ -31,5 +37,24 @@ public class ConsoleHandler {
             l+
             "=========================\n"
         );      
+    }
+
+    private void initPromptWindow() {
+        
+    }
+    
+    public void dProgramHeader(){
+        System.out.println("=====| CityGame |=====");
+        System.out.println("= Development by GDR =");
+        System.out.println("=    v0 dev build    =");
+        System.out.println("======================");
+        this.window.writeln("=====| CityGame |=====");
+        this.window.writeln("= Development by GDR =");
+        this.window.writeln("=    v0 dev build    =");
+        this.window.writeln("======================");
+    }
+    
+    public Prompt getPromptWindow(){
+        return this.window;
     }
 }
